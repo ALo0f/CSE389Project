cd0e6d02d7aa3681c780db49c00064e5478407ed
tianyi <twu116@syr.edu>
2020-10-23 21:55:34 -0400

    add details to logging functions, leave update and save for future work

--- Pr0j3ct/logging.py
+++ Pr0j3ct/logging.py
@@ -1,24 +1,40 @@
 # logging.py
 # implements Server Logger class
+from datetime import datetime
 
 class Logger:
     def __init__(self, caller):
         self.caller = caller
+        self.cache = []
+        self.maxCacheSize = 100
 
     def info(self, message):
         """
         Log information level message
         """
-        pass
+        print("[{} {} {}] {}".format("INFO", datetime.now().strftime("%d/%m/%Y %H:%M:%S"), self.caller, message))
+        
 
     def warn(self, message):
         """
         Log warning level message
         """
-        pass
+        print("[{} {} {}] {}".format("WARN", datetime.now().strftime("%d/%m/%Y %H:%M:%S"), self.caller, message))
 
     def error(self, message):
         """
         Log error level message
+        """
+        print("[{} {} {}] {}".format("ERROR", datetime.now().strftime("%d/%m/%Y %H:%M:%S"), self.caller, message))
+
+    def _update(self):
+        """
+        Update cache, check cache if exceeds the default value, if yes save it to local space.
+        """
+        pass
+
+    def _save(self):
+        """
+        Save log message to local space
         """
         pass
